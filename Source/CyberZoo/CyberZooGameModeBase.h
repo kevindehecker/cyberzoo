c572d3f8e85be33cf6b6b3dcd2fb172271a6b4a7
kevindehecker <kevindehecker@hotmail.com>
2017-05-13 21:24:10 +0200

    Initial commit

---
+++ Source/CyberZoo/CyberZooGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "CyberZooGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERZOO_API ACyberZooGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
